9a588eed1cb010fb394e358c5e6a924c8cbb33c9
tanvikumari027 <tanvikumari027@gmail.com>
2025-09-28 13:06:29 +0530

    P11.c

--- P11.c
+++ P11.c
@@ -2,8 +2,10 @@
 int main(){
     float operand1,operand2,result;
     char operator;
+    
     printf("enter the operator(+,-,*,/)= ");
     scanf("%c", &operator);
+    
     printf("Enter the first operand= ");
     scanf("%f",&operand1);
     printf("Enter the second operand= ");
@@ -15,20 +17,25 @@ int main(){
         result=operand1+operand2;
         printf("%.2f",result);
         break;
+        
     case '-':
         result=operand1-operand2;
         printf("%.2f",result);
         break;
+        
     case '*':
         result=operand1*operand2;
         printf("%.2f",result);
         break;
+        
     case '/':
         result=operand1/operand2;
         printf("%.2f",result);
         break;
+        
     default:
         printf("wrong operator");
         break;
     }
+
 }
