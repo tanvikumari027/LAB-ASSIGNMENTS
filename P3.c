3622b512ebfcbe6a0e83fae340314e7f32a07f0a
tanvikumari027 <tanvikumari027@gmail.com>
2025-10-02 22:32:24 +0530

    P3.c

--- P3.c
+++ P3.c
@@ -1,22 +1,22 @@
 //area and circumference of circle
 #include<stdio.h>
-#define _USE_MATH_DEFINES
 #include<math.h>
 
 int main(){
     
-    float radius,area,circumference;
+    float radius , area , circumference;
 
-    printf("Enter the radius= ");
-    scanf("%f",&radius);
+    printf("Enter the radius=  ");
+    scanf("%f" , &radius );
 
-    area= M_PI *pow(radius,2);
-    circumference=2*M_PI*radius;
+    area = 3.14 * pow( radius , 2 );
+    circumference = 2 * 3.14 * radius;
 
-    printf("The circumference of circle is= %.2f\n",circumference);
-    printf("The area of circle is= %.2f\n",area);
+    printf("The circumference of circle is=  %.2f\n" , circumference);
+    printf("The area of circle is=  %.2f\n" , area);
     
     return 0;
 
 
 }
+
