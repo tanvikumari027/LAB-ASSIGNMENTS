7453f78c625a1ac91b1322b5a9920e397dce13cc
tanvikumari027 <tanvikumari027@gmail.com>
2025-10-02 20:03:32 +0530

    P12.c

--- P12.c
+++ P12.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main(){
+    
     int a;
     float sum;
+    
     printf("Enter the number");
     scanf("%d",&a);
+    
     sum=(a*(a+1))/2.0;
+    
     printf("sum= %.2f",sum);
+
 
 }
