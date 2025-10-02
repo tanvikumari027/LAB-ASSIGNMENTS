075aca67bdc0e52e00f5d2b433296fd2f07b51ab
tanvikumari027 <tanvikumari027@gmail.com>
2025-10-02 22:28:50 +0530

    P9.c

--- P9.c
+++ P9.c
@@ -2,14 +2,18 @@
 int main()
 {
     int year;
+    
     printf("Enter a year: ");
     scanf("%d", &year);
+    
     if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) 
     {
         printf("It's a leap year");
     }
+        
     else
     {
         printf("It's not a leap year");
     }
+
 }
