7311bb7c55f98a6608a59525d373d060a1f483ad
tanvikumari027 <tanvikumari027@gmail.com>
2025-10-02 22:30:24 +0530

    P4.c

--- p4.c
+++ p4.c
@@ -1,9 +1,14 @@
 //accepts the temperature in Centigrade and converts into Fahrenheit 
 #include<stdio.h>
 int main(){
-    float centigrade,fahrenheit;
+    
+    float tempCentigrade,tempFahrenheit;
+    
     printf("Enter temperature in Celsius: ");
-    scanf("%f", &centigrade);
-    fahrenheit = (centigrade * 9.0 / 5.0) + 32;
-    printf("Temperature in Fahrenheit = %.2f\n", fahrenheit);
+    scanf("%f", &tempCentigrade);
+    
+    tempFahrenheit = (tempCentigrade * 9.0 / 5.0) + 32;
+    
+    printf("Temperature in Fahrenheit =  %.2f\n", tempFahrenheit);
+
 }
