3e8f1a33f245a29faa5dc2a792b345f65a8f0b65
tanvikumari027 <tanvikumari027@gmail.com>
2025-10-02 21:30:01 +0530

    P5.c

--- P5.c
+++ P5.c
@@ -1,16 +1,5 @@
-/*swaps values of two variables using a third variable.
-#include<stdio.h>
-int main(){
-    int firstNum,secondNum,a;
-    printf("Enter the first number= ");
-    scanf("%d",&firstNum);
-    printf("Enter the second number= ");
-    scanf("%d",&secondNum);
-    a=firstNum;
-    firstNum=secondNum;
-    secondNum=a;
-    printf("the number after swapping=  firstNum = %d, secondNum = %d\n", firstNum, secondNum);
-}*/
+//swaps values of two variables using a third variable.
+
 #include <stdio.h>
 int main() {
     int firstNum, secondNum, a;
@@ -28,3 +17,4 @@ int main() {
 
     printf("The numbers after swapping= %d  %d\n", firstNum, secondNum);
 }
+
